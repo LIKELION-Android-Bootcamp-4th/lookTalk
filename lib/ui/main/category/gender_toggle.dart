d7715c1c8fe10d91fa7578ae8458b44a3b3f0f48
rlaaudgjs2 <68803644+rlaaudgjs2@users.noreply.github.com>
2025-07-01 00:14:42 +0900

    category MVVM

---
+++ lib/ui/main/category/gender_toggle.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter/material.dart';
+
+class GenderToggle extends StatelessWidget{
+  final String selectedGender;
+  final Function(String) onSelectedButton;
+
+  const GenderToggle({required this.selectedGender, required this.onSelectedButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: GestureDetector(
+          onTap: () => onSelectedButton('남자'),
+          child: Container(
+            width: 131,
+            height: 29,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: selectedGender == '남자' ? Colors.lightBlueAccent : Colors.white,
+              borderRadius: BorderRadius.circular(14),
+            ),
+            child: Text("남자",
+              style: TextStyle(
+                color: selectedGender == '남자' ? Colors.black : Colors.grey[500],
+              ),
+            ),
+
+          ),
+        ),
+        ),
+        Expanded(child: GestureDetector(
+          onTap: () => onSelectedButton('여자'),
+          child: Container(
+            width: 131,
+            height: 29,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: selectedGender == '여자' ? Colors.lightBlueAccent: Colors.white,
+              borderRadius: BorderRadius.circular(14),
+
+            ),
+            child: Text("여자",
+    style: TextStyle(
+    color: selectedGender == '여자' ? Colors.black : Colors.grey[500],
+    ),),
+          ),
+        ))
+      ],
+    );
+  }
+}
