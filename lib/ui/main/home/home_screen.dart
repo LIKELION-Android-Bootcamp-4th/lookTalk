a19bb0c5b6fae417609fa71789af99ca5818df3d
msj9965 <msj9965@hotmail.com>
2025-06-30 21:09:15 +0900

    폰트 적용

--- lib/ui/main/home/home_screen.dart
+++ lib/ui/main/home/home_screen.dart
@@ -1,9 +1,24 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '헤드라인 텍스트입니다',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(height: 8),
+            Text('본문 텍스트입니다', style: Theme.of(context).textTheme.bodyLarge),
+            const SizedBox(height: 8),
+            Text('설명 텍스트입니다', style: Theme.of(context).textTheme.bodySmall),
+          ],
+        ),
+      ),
+    );
   }
 }
