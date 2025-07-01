2e4f534f9718c1a35afbb02df57ee611b98df1f5
jessica3579 <soojilee3579@naver.com>
2025-07-01 13:26:31 +0900

    Feat: 공통 gap 상수 추가

---
+++ lib/ui/common/const/gap.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// 높이 간격
+const gap4 = SizedBox(height: 4);
+const gap8 = SizedBox(height: 8);
+const gap12 = SizedBox(height: 12);
+const gap16 = SizedBox(height: 16);
+const gap24 = SizedBox(height: 24);
+const gap32 = SizedBox(height: 32);
+
+// 너비 간격
+const gapW4 = SizedBox(width: 4);
+const gapW8 = SizedBox(width: 8);
+const gapW12 = SizedBox(width: 12);
+const gapW16 = SizedBox(width: 16);
+const gapW24 = SizedBox(width: 24);
