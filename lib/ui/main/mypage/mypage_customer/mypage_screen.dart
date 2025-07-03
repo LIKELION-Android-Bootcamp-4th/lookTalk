33fbbc9f61268fe33540a3546bdf0262ef5ea88b
rlaaudgjs2 <68803644+rlaaudgjs2@users.noreply.github.com>
2025-07-03 23:41:49 +0900

    profile

--- lib/ui/main/mypage/mypage_customer/mypage_screen.dart
+++ lib/ui/main/mypage/mypage_customer/mypage_screen.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:look_talk/ui/common/component/app_bar/app_bar_search_cart.dart';
 import 'package:look_talk/ui/common/const/colors.dart';
 import 'package:look_talk/ui/common/const/text_sizes.dart';
 import 'package:look_talk/ui/common/const/gap.dart';
+import 'package:look_talk/ui/main/mypage/mypage_customer/alter_member.dart';
 import 'package:look_talk/ui/main/mypage/mypage_screen_product_manage.dart';
 
-class MyPageScreenSeller extends StatelessWidget {
-  const MyPageScreenSeller({super.key});
+class MyPageScreenCustomer extends StatelessWidget {
+  const MyPageScreenCustomer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("마이페이지"),
-        titleTextStyle: TextStyle(
-            fontFamily: "NanumSquareRoundB.ttf",
-            fontSize: TextSizes.headline,
-            color: AppColors.black,
-            fontWeight: FontWeight.w300
-        ),
-        ),
+      appBar: AppBarSearchCart(title: "마이페이지",),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
         child: Column(
@@ -26,31 +21,40 @@ class MyPageScreenSeller extends StatelessWidget {
             // 프로필 사진/ 이름 영역
             Row(
               children: [
+
                 CircleAvatar(
                   radius: 60,
                   backgroundColor: Colors.grey[400],
                   child: Icon(Icons.person, size: 60, color: AppColors.white),
                 ),
                 gapW16,
-                Text(
-                  '회사명',
-                  style: TextStyle(
-                    fontSize: TextSizes.headline,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_)=> AlterMember()));
+                  },
+                  child: Row(
+                    children: [
+                      Text(
+                        '이름(닉네임)',
+                        style: TextStyle(
+                          fontSize: TextSizes.headline,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.chevron_right)
+                    ],
+                  ) ,
+
+
+                )
+
               ],
             ),
             gap32,
-
-            _MyPageMenu(title: '상품 조회 / 등록', onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const MyPageProductManageScreen()),
-              );
-            }),
+            const _MyPageMenu(title: '최근 본 상품'),
             gap16,
-            const _MyPageMenu(title: '주문 조회',),
+            const _MyPageMenu(title: '주문/교환/반품/취소 내역',),
             gap16,
             const _MyPageMenu(title: '공지사항'),
             gap16,
@@ -82,10 +86,10 @@ class _MyPageMenu extends StatelessWidget {
         child: Text(
           title,
           style: const TextStyle(
-            fontFamily: "NanumSquareRoundB.ttf",
-            fontSize: TextSizes.body,
-            color: AppColors.black,
-            fontWeight: FontWeight.bold
+              fontFamily: "NanumSquareRoundB.ttf",
+              fontSize: TextSizes.body,
+              color: AppColors.black,
+              fontWeight: FontWeight.bold
           ),
         ),
       ),
