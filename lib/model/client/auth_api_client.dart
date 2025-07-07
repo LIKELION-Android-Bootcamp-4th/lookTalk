de55b5287899231c71dac7df4d610edf04308498
jessica3579 <soojilee3579@naver.com>
2025-07-07 09:17:29 +0900

    WIP: 충돌 해결중

--- lib/model/client/auth_api_client.dart
+++ lib/model/client/auth_api_client.dart
@@ -1,10 +1,7 @@
 import 'package:dio/dio.dart';
-import 'package:look_talk/core/network/dio_client.dart';
-import 'package:look_talk/core/network/token_storage.dart';
 import 'package:look_talk/model/entity/response/login_response.dart';
 
 import '../../core/network/api_result.dart';
-import '../../core/network/endpoints.dart';
 import '../entity/request/social_login_request.dart';
 
 // dio 를 이용해 서버 api 를 직접 호출
@@ -17,7 +14,7 @@ class AuthApiClient {
     required SocialLoginRequest request,
   }) async {
     final response = await _dio.post(
-      Endpoints.socialLogin,
+      AuthEndpoints.socialLogin,
       data: request.toJson(),
     );
     return ApiResult.fromResponse(response, LoginResponse.fromJson);
