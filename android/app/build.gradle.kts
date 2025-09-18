6c5662d66072e82154a1f7766256fa894349e1ad
suji lee <soojilee3579@naver.com>
2025-09-18 14:33:40 +0900

    Fix: 패키지명 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.look_talk"
+    namespace = "com.bone_soup.look_talk"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13599879"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.look_talk"
+        applicationId = "com.bone_soup.look_talk"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
